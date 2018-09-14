7f67b47a532de921b0f20da169af8ff5ece45447
THOTA KALYAN <THOTA KALYAN@DESKTOP-V4VTGQT>
2018-09-14 19:18:58 +0530

    committed

--- Log4j/src/main/java/Test1.java
+++ Log4j/src/main/java/Test1.java
@@ -9,6 +9,6 @@ System.out.println("***************");
 System.out.println("%%%%%%%%%%%%%%%%%%%%%%%");
 System.out.println("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqq");
 System.out.println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-System.out.println("bbbbbbbbbbbbbbbbb{{{{{{{}}}}}}}}bbbbbbbbbbbbbbbbbbbbb");
+System.out.println("bbbbbbbbbbbbbbbbb{{{{{{{.}}}}}}}}bbbbbbbbbbbbbbbbbbbbb");
 }
 }
